29ec0dc1fd7728769b170fe67eba06d5e7ec9503
mustafadmrkrn <mustafa.demirkiran@outlook.com.tr>
2023-04-27 16:06:52 +0300

    Python Examples

---
+++ DegiskenTurleri.py
@@ -0,0 +1,8 @@
+musteriAdi = "Ali"
+musteriSoyad = "Yılmaz"
+musteriAdSoyad = musteriAdi + " " + musteriSoyad
+musteriCinsiyet = True #Erkek
+musteriTcKimlik = "1232346732"
+musteriDogumYili = 2013
+musteriAdresi = "İstanbul Kadıköy"
+musteriYasi = 2023 - musteriDogumYili
