f8465b95925c382f585779c922acf6032c984f87
mustafadmrkrn <99756573+mustafadmrkrn@users.noreply.github.com>
2023-12-18 12:20:14 +0300

    deneme

---
+++ Python_Lessons/27_TkinterFrameRenk.py
@@ -0,0 +1,14 @@
+﻿from tkinter import *
+
+pencere = Tk()
+pencere.geometry("200x105")
+pencere.title("Bahçeşehir Kolej")
+kirmiziFrame = Frame(bg="Red", width=220, height=60)
+kirmiziFrame.place(x=10,y=10)
+maviFrame = Frame(bg="Blue",width=220,height=60)
+maviFrame.place(x=10, y=80)
+b1=Button(kirmiziFrame, text="Button1 Kırmızı")
+b1.place(x=0,y=0)
+b2=Button(maviFrame,text="Button2 Mavi")
+b2.place(x=0,y=0)
+pencere.mainloop()
